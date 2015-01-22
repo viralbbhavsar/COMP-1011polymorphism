7f24fa521171aab882ad472665f3ec7b362d9a8d
viralbbhavsar <viralbbhavsar@gmail.com>
2015-01-22 15:33:41 -0500

    Created project class

---
+++ src/Project.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Viral
+ *
+ */
+public class Project {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
